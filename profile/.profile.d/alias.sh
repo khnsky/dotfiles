8f5851a342610404f3fd49f40b9e6acd5b01ddb1
Piotrek Kochański <khnsky@khnsky.cc>
2021-06-21 22:08:07 +0200

    add function for disassembly and alias to show webcam

--- profile/.profile.d/alias.sh
+++ profile/.profile.d/alias.sh
@@ -8,3 +8,9 @@ alias mv='mv -i'
 alias ln='ln -i'
 
 alias df='df -h'
+
+disass() {
+    objdump -d -M intel "$@" | highlight -S asm -O truecolor
+}
+
+alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed --vf=hflip'
