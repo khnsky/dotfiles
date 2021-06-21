7622572ff65aa807e3021ee64ccefde1781dd822
Piotrek Kochański <khnsky@khnsky.cc>
2021-06-21 22:07:10 +0200

    add safety aliases

--- profile/.profile.d/alias.sh
+++ profile/.profile.d/alias.sh
@@ -2,9 +2,9 @@ alias sudo='sudo '                      # expand aliases when using with sudo
 
 alias ping1='ping -c1'
 
-#alias cp='cp -ir'
-#alias rm='rm -I --preserve-root'
-#alias mv='mv -i'
-#alias ln='ln -i'
+alias cp='cp -ir'
+alias rm='rm -I --preserve-root'
+alias mv='mv -i'
+alias ln='ln -i'
 
-#alias df='df -h'
+alias df='df -h'
