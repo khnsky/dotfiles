6af05e0def52d68e734147000247d33bf477a0fe
Piotrek Kochański <khnsky@khnsky.cc>
2021-06-26 20:55:28 +0200

    bug fixes and performance improvements

--- install.sh
+++ install.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
 
-WD=$(pwd)
-DIR=$(dirname "$0")
+# directory the script resides in - the root directory of the repo
+SCRIPT_DIR=$(dirname -- "$0")
 
-if ! cd "$DIR"; then
+if ! cd -- "$SCRIPT_DIR"; then
     echo 'entering script dir failed' 1>&2
     exit 1
 fi
 
-FILES=${*:-*}
-
-echo "installing: $FILES"
-
-for f in $FILES; do
-    [ ! -d "$f" ] && continue
-
-    echo "=> installing $f"
-    if [ -x "$f/install.sh" ]; then
-        "$f/install.sh"
-    else
-        stow -d "$DIR" -t "$HOME" "$f"
-    fi
-
-    [ $? -ne 0 ] && echo "$f install failed ($?)" 1>&2
-done
-
-if ! cd "$WD"; then
-    echo 'returning to working directory failed' 1>&2
-    exit 1
+# if no arguments were provided use all files in current directory
+if [ $# -eq 0 ]; then
+    set -- *
 fi
+
+echo "installing: $*"
+
+install() {
+    echo "=> installing $1"
+    # is install script exists run that, else stow to home
+    if [ -x "$1/install.sh" ]; then
+        "$1"/install.sh
+    else
+        stow -t "$HOME" "$1"
+    fi
+}
+
+for package; do
+    # if package is not a directory skip
+    [ ! -d "$package" ] && continue
+
+    if ! install "$package"; then
+        echo "$package: install failed ($?)" 1>&2
+    fi
+done
+unset package
