adabdcf1675c9006caa45dcdd62280e0b3308b31
Piotrek Kochański <khnsky@khnsky.cc>
2026-02-17 00:03:30 +0100

    Add lua_ls configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -252,4 +252,31 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = on_attach,
 })
 
-vim.lsp.enable { 'clangd', 'pyright' }
+vim.lsp.enable { 'clangd', 'pyright', 'lua_ls' }
+vim.lsp.config('lua_ls', {
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath('config') and (
+                vim.uv.fs_stat(path..'/.luarc.json') or
+                vim.uv.fs_stat(path..'/.luarc.jsonc')
+            ) then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    "${3rd}/luv/library",
+                }
+            }
+        })
+    end,
+    settings = {
+        Lua = {}
+    },
+})
